8da7caf7591e69116adf4d4c7df34bcca1234b4d
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2023-12-12 16:46:43 +0330

    Show weather information of a city

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "5adc4078b4c291a05ca4fc8cfc68d770";
 const searchInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
-
+const weatherContainer=document.getElementById("weather")
 const getCurrentWeatherByName = async (city) => {
   const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
   const response = await fetch(url);
@@ -15,7 +15,22 @@ const searchHandler = async () => {
     alert("please enter city name");
   }
   const currentData = await getCurrentWeatherByName(cityName);
-  console.log(currentData);
+  renderCurrentWeather(currentData)
+};
+const renderCurrentWeather = (data) => {
+    const weatherJSX = `
+    <h1>${data.name} , ${data.sys.country}</h1>
+    <div id="main">
+    <img alt="weather icon" src="https://api.openweathermap.org/img/w/${data.weather[0].icon}.png">
+    <span>${data.weather[0].main}</span>
+    <p>${Math.round(data.main.temp)}°C</p>
+    </div>
+    <div id="info">
+    <p>Humidity:<span>${data.main.humidity}%</span></p>
+    <p>Wind Speed:<span>${data.wind.speed}m/s</span></p>
+    </div>
+    `;
+  weatherContainer.innerHTML = weatherJSX;
 };
 
 searchButton.addEventListener("click", searchHandler);
